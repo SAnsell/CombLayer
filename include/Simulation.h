8aa0237923256be79916a779a8c20eff3af13894
SAnsell <stuart.ansell@esss.se>
2015-05-22 15:58:18 +0000

    Bug/history issue : removed redundent curObjPtr from Simulation   -- this cannot be used like this because of the way the singletons   interact. I.e. Simuation A can invalidate the curObjPtr of Simulation   B. Now we have SimTrack that deals with all of the problem. BUT   the legacy remains in bits of the code -- but not any longer.
      Further I have removed the use of boost::bind from Simulation.

--- include/Simulation.h
+++ include/Simulation.h
@@ -1,9 +1,9 @@
 /********************************************************************* 
-  CombLayer : MNCPX Input builder
+  CombLayer : MCNP(X) Input builder
  
  * File:   include/Simulation.h
  *
- * Copyright (c) 2004-2014 by Stuart Ansell
+ * Copyright (c) 2004-2015 by Stuart Ansell
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -85,7 +85,6 @@ class Simulation
   int CNum;                             ///< Number of complementary components
   FuncDataBase DB;                      ///< DataBase of variables
   ModelSupport::ObjSurfMap* OSMPtr;     ///< Object surface map [if required]
-  MonteCarlo::Object* curObjPtr;        ///< Last find pointer
 
   TransTYPE TList;        ///< Transforms List (key=Transform)
 
