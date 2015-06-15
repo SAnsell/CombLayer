44cfa9b9efb33177704c83b37282008108d194a8
Stuart Ansell <xansell@gmail.com>
2015-06-15 21:33:18 +0100

    Fix of bug to make master build

--- System/process/MainProcess.cxx
+++ System/process/MainProcess.cxx
@@ -214,7 +214,7 @@ createInputs(inputParam& IParam)
   IParam.regItem("defaultConfig","defaultConfig");
   IParam.regDefItem<std::string>("dc","doseCalc",1,"InternalDOSE");
   IParam.regFlag("e","endf");
-  IParam.regMulti<std::string>("eng","engineering",10000,0);
+  IParam.regMulti("eng","engineering",10000,0);
   IParam.regMulti("E","exclude",10000,1);
   IParam.regDefItem<double>("electron","electron",1,-1.0);
   IParam.regFlag("help","helf");
