b3b0b553e6636281c4188cd90b9eaedc43af866d
Konstantin Batkov <konstantin.batkov@esss.se>
2015-11-05 12:49:25 +0100

    Top/LowCapModHeight1 and Top/LowCapModThick1 changed from 0.6 to 0.3 as recommended by Yannick (via Daniel)

--- Model/essBuild/essVariables.cxx
+++ Model/essBuild/essVariables.cxx
@@ -1241,9 +1241,9 @@ EssButterflyModerator(FuncDataBase& Control)
   Control.addVariable("LowCapModDepth0", 0.5);
   Control.addVariable("LowCapModRadius0",19.5);
   Control.addVariable("LowCapModMat0","H2O");
-  Control.addVariable("LowCapModHeight1",0.6);
+  Control.addVariable("LowCapModHeight1",0.3);
   Control.addVariable("LowCapModDepth1",0.0);
-  Control.addVariable("LowCapModRadius1",0.6);
+  Control.addVariable("LowCapModThick1",0.3);
   Control.addVariable("LowCapModMat1","Aluminium");
   Control.addVariable("LowCapModHeight2",0.0);
   Control.addVariable("LowCapModDepth2",0.0);
@@ -1269,9 +1269,9 @@ EssButterflyModerator(FuncDataBase& Control)
   Control.addVariable("TopCapModDepth0", 0.5);
   Control.addVariable("TopCapModRadius0",19.5);
   Control.addVariable("TopCapModMat0","H2O");
-  Control.addVariable("TopCapModHeight1",0.6);
+  Control.addVariable("TopCapModHeight1",0.3);
   Control.addVariable("TopCapModDepth1",0.0);
-  Control.addVariable("TopCapModRadius1",0.6);
+  Control.addVariable("TopCapModThick1",0.3);
   Control.addVariable("TopCapModMat1","Aluminium");
   Control.addVariable("TopCapModHeight2",0.0);
   Control.addVariable("TopCapModDepth2",0.0);
