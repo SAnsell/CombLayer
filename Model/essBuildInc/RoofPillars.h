a7f796a62d6077423a6cd90f4ac28ca445b7f812
SAnsell <stuart.ansell@esss.se>
2015-11-26 17:28:34 +0000

    Adding the start of roof pillars

---
+++ Model/essBuildInc/RoofPillars.h
@@ -0,0 +1,80 @@
+/********************************************************************* 
+  CombLayer : MCNP(X) Input builder
+ 
+ * File:   essBuildInc/RoofPillars.h
+ *
+ * Copyright (c) 2004-2015 by Stuart Ansell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *
+ ****************************************************************************/
+#ifndef essSystem_RoofPillars_h
+#define essSystem_RoofPillars_h
+
+class Simulation;
+
+namespace essSystem
+{
+  class Bunker;
+  
+/*!
+  \class RoofPillars
+  \version 1.0
+  \author S. Ansell
+  \date November 2015
+  \brief Roof pillar to hold up bunker roof
+*/
+
+class RoofPillars : public attachSystem::FixedComp,
+  public attachSystem::CellMap
+{
+ private:
+   
+  const int rodIndex;           ///< Index of surface offset
+  int cellIndex;                ///< Cell index
+
+  /// Relative point for each centre
+  std::vector<Geometry::Vec3D> CentPoint;
+
+  double radius;                ///< Radius for each support
+  int mat;                      ///< Matieral
+  
+  
+  void populate(const FuncDataBase&);
+  void createUnitVector(const attachSystem::FixedComp&,
+			const attachSystem::FixedComp&,
+			const long int,const bool);
+
+  void createSurfaces();
+  void createLinks();
+  void createObjects(Simulation&,const attachSystem::FixedComp&,
+		     const long int);
+  
+ public:
+
+  RoofPillars(const std::string&);
+  RoofPillars(const RoofPillars&);
+  RoofPillars& operator=(const RoofPillars&);
+  virtual ~RoofPillars();
+
+
+  void createAll(Simulation&,const Bunker&);
+
+};
+
+}
+
+#endif
+ 
+
