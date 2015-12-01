d8d041d15cb5044006c45c495473709bcb9e9bd9
Konstantin Batkov <konstantin.batkov@esss.se>
2015-12-01 13:10:50 +0100

    NBrickSectors set to 0

--- Model/essBuild/essVariables.cxx
+++ Model/essBuild/essVariables.cxx
@@ -130,7 +130,7 @@ EssWheel(FuncDataBase& Control)
   Control.addVariable("BilbaoWheelInnerStructureBrickGapMat","Helium"); // shoud be the same as BilbaoWheelHeMat
 
   Control.addVariable("BilbaoWheelInnerStructureNSectors", 36);
-  Control.addVariable("BilbaoWheelInnerStructureNBrickSectors", 3);
+  Control.addVariable("BilbaoWheelInnerStructureNBrickSectors", 0);
   Control.addVariable("BilbaoWheelInnerStructureSectorSepThick", 1.0);
   Control.addVariable("BilbaoWheelInnerStructureSectorSepMat", "SS316L785");
 
