2d665369f1133b2f04362c756826427ec3d82e5f
Konstantin Batkov <konstantin.batkov@esss.se>
2016-02-02 10:12:09 +0100

    WIP: MidWaterLength adjusted to put the focal points on their places

--- Model/essBuild/essVariables.cxx
+++ Model/essBuild/essVariables.cxx
@@ -1163,7 +1163,7 @@ EssButterflyModerator(FuncDataBase& Control)
   Control.addVariable("TopFlyMidWaterCutLayer",3);
   Control.addVariable("TopFlyMidWaterMidYStep",4.635);
   Control.addVariable("TopFlyMidWaterMidAngle",90);
-  Control.addVariable("TopFlyMidWaterLength",10.98);
+  Control.addVariable("TopFlyMidWaterLength",11.4);
  
 
   Control.addVariable("TopFlyMidWaterWallThick",0.2);
