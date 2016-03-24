c4a02ae6ea979ea144b54a9e92acb670646c6ff1
Konstantin Batkov <konstantin.batkov@esss.se>
2016-03-24 15:10:31 +0100

    WIP: twister plug frame: inside sector connects with twister shaft with tangential planes

--- Model/essBuild/TwisterModule.cxx
+++ Model/essBuild/TwisterModule.cxx
@@ -215,9 +215,6 @@ TwisterModule::createSurfaces()
 {
   ELog::RegMethod RegA("TwisterModule","createSurfaces");
 
-  // dividing plane
-  ModelSupport::buildPlane(SMap,tIndex+2,Origin,Y);
-
   ModelSupport::buildPlane(SMap,tIndex+5,Origin-Z*plugFrameDepth,Z);
   ModelSupport::buildPlane(SMap,tIndex+6,Origin+Z*plugFrameHeight,Z);
   ModelSupport::buildPlane(SMap,tIndex+16,Origin+Z*(plugFrameHeight+plugFrameWallThick+shaftHeight),Z);
@@ -231,11 +228,25 @@ TwisterModule::createSurfaces()
   ModelSupport::buildCylinder(SMap,tIndex+27,Origin,Z,plugFrameRadius);
   ModelSupport::buildCylinder(SMap,tIndex+37,Origin,Z,plugFrameRadius+plugFrameWallThick);
 
-  ModelSupport::buildPlaneRotAxis(SMap,tIndex+1,Origin,X,-Z,plugFrameAngle/2.0);
-  ModelSupport::buildPlaneRotAxis(SMap,tIndex+11,Origin,X,-Z,-plugFrameAngle/2.0);
+  // sector walls
+  const double angle = plugFrameAngle/2.0*M_PI/180;
+  double R = shaftRadius;
 
-  ModelSupport::buildPlaneRotAxis(SMap,tIndex+21,Origin+X*shaftWallThick,X,-Z,plugFrameAngle/2.0);
-  ModelSupport::buildPlaneRotAxis(SMap,tIndex+31,Origin-X*shaftWallThick,X,-Z,-plugFrameAngle/2.0);
+  ModelSupport::buildPlaneRotAxis(SMap,tIndex+1,
+				  Origin-Y*R*sin(angle)+X*R*cos(angle),
+				  X,-Z,plugFrameAngle/2.0);
+  ModelSupport::buildPlaneRotAxis(SMap,tIndex+11,
+				  Origin-Y*R*sin(angle)-X*R*cos(angle),
+				  X,-Z,-plugFrameAngle/2.0);
+
+  R += shaftWallThick;
+  ModelSupport::buildPlane(SMap,tIndex+2,Origin-Y*(R*sin(angle)),Y); // dividing plane
+  ModelSupport::buildPlaneRotAxis(SMap,tIndex+21,
+				  Origin-Y*R*sin(angle)+X*R*cos(angle),
+				  X,-Z,plugFrameAngle/2.0);
+  ModelSupport::buildPlaneRotAxis(SMap,tIndex+31,
+				  Origin-Y*R*sin(angle)-X*R*cos(angle),
+				  X,-Z,-plugFrameAngle/2.0);
 
   
   return; 
@@ -260,13 +271,13 @@ TwisterModule::createObjects(Simulation& System)
   setCell("lowBe",cellIndex-1);
 
   // plug frame
-  Out=ModelSupport::getComposite(SMap,tIndex," -27 5 -6 17 -1 11"); //  inside sector
+  Out=ModelSupport::getComposite(SMap,tIndex," -27 5 -6 17 2 -1 11"); //  inside sector
   System.addCell(MonteCarlo::Qhull(cellIndex++,plugFrameMat,0.0,Out));
 
   Out=ModelSupport::getComposite(SMap,tIndex," -27 5 -6 17 2 -21 1"); //  inside sector wall
   System.addCell(MonteCarlo::Qhull(cellIndex++,plugFrameMat,0.0,Out));
 
-  Out=ModelSupport::getComposite(SMap,tIndex," -27 5 -6 17 (21:-31)"); // outside sector
+  Out=ModelSupport::getComposite(SMap,tIndex," -27 5 -6 17 (-2:21:-31)"); // outside sector
   System.addCell(MonteCarlo::Qhull(cellIndex++,plugFrameMat,0.0,Out));
 
   Out=ModelSupport::getComposite(SMap,tIndex," -27 5 -6 17 2 31 -11"); //  inside sector wall
