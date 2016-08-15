3a46f9e2be77457ae318bef03c042d06afc230de
Konstantin Batkov <konstantin.batkov@esss.se>
2016-08-15 12:24:15 +0200

    Helium instead of helium material is used (otherwise, if 'helium' is set then 'Al' is being used - this is a bug)

--- Model/essBuild/targetVariables.cxx
+++ Model/essBuild/targetVariables.cxx
@@ -265,21 +265,21 @@ EssProtonBeam(FuncDataBase& Control)
   Control.addVariable("ProtonTubeLength1",148.25); //from mod centre leftside
   Control.addVariable("ProtonTubeZcut1",3.7);  // to have the same height as BeRef::targetVoid
   Control.addVariable("ProtonTubeWallThick1",0.0);
-  Control.addVariable("ProtonTubeInnerMat1","helium");  // mat : 2000
+  Control.addVariable("ProtonTubeInnerMat1","Helium");  // mat : 2000
   Control.addVariable("ProtonTubeWallMat1","CastIron"); // mat : 26316
 
   Control.addVariable("ProtonTubeRadius2",15.0);  // as in TSM141108V2003
   Control.addVariable("ProtonTubeLength2",200.0);
   Control.addVariable("ProtonTubeZcut2",0.0); 
   Control.addVariable("ProtonTubeWallThick2",1.0);
-  Control.addVariable("ProtonTubeInnerMat2","helium");  // mat : 2000
+  Control.addVariable("ProtonTubeInnerMat2","Helium");  // mat : 2000
   Control.addVariable("ProtonTubeWallMat2","CastIron"); // mat : 26316
 
   Control.addVariable("ProtonTubeRadius3",15.0);  // as in TSM141108V2003
   Control.addVariable("ProtonTubeLength3",127.5);
   Control.addVariable("ProtonTubeZcut3",0.0);
   Control.addVariable("ProtonTubeWallThick3",1.0);
-  Control.addVariable("ProtonTubeInnerMat3","helium");   // mat : 2000
+  Control.addVariable("ProtonTubeInnerMat3","Helium");   // mat : 2000
   Control.addVariable("ProtonTubeWallMat3","CastIron");  // mat : 26316
 
   Control.addVariable("ProtonTubeRadius4",15.0); // as in TSM141108V2003
