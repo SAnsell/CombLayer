c9fcb4093e5ff49c63fb45aa1ca0f84ae1d4f959
Stuart Ansell <xansell@gmail.com>
2016-08-26 07:48:21 +0100

    Error with makeD4c that prevented a full build --   caused by yesterdays update on CylSample

--- Model/d4cModel/makeD4C.cxx
+++ Model/d4cModel/makeD4C.cxx
@@ -1,9 +1,9 @@
 /********************************************************************* 
-  CombLayer : MNCPX Input builder
+  CombLayer : MCNP(X) Input builder
  
  * File:   d4cModel/makeD4C.cxx
  *
- * Copyright (c) 2004-2015 by Stuart Ansell
+ * Copyright (c) 2004-2016 by Stuart Ansell
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -62,6 +62,7 @@
 #include "Simulation.h"
 #include "LinkUnit.h"
 #include "FixedComp.h"
+#include "FixedOffset.h"
 #include "ContainedComp.h"
 #include "ContainedGroup.h"
 #include "World.h"
