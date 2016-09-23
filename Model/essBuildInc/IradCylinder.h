0382b12ea42623f3786793ce5a70bf9550a6cae3
Stuart Ansell <xansell@gmail.com>
2016-09-23 23:34:58 +0100

    Minor issues with IradCyclinder
       -- copy constructor
       -- sample exit point

--- Model/essBuildInc/IradCylinder.h
+++ Model/essBuildInc/IradCylinder.h
@@ -43,6 +43,8 @@ class IradCylinder : public attachSystem::ContainedComp,
   const int iradIndex;            ///< Index of surface offset
   int cellIndex;                  ///< Cell index
 
+  int sampleActive;               ///< samples are not built if set to zero
+  
   double radius;                  ///< Main radius [from built object]
   double length;                  ///< Length of cylinder
   std::vector<double> wallThick;  ///< Wall thicknesses
@@ -51,7 +53,7 @@ class IradCylinder : public attachSystem::ContainedComp,
   int mat;                        ///< Main material
   std::vector<int> wallMat;       ///< Wall material
 
-  int    sampleActive;            ///< samples are not built if set to zero
+
   double sampleX;                 ///< Sample X step     
   double sampleY;                 ///< Sample Z step     
   double sampleZ;                 ///< Sample Z step
