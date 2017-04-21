be4540be4c3202fef4a09f92ceba45cfbb5bb5e8
SAnsell <stuart.ansell@esss.se>
2017-04-21 12:03:30 +0200

    Correcting failure to delete local cone povray code

--- System/geometry/Cone.cxx
+++ System/geometry/Cone.cxx
@@ -429,16 +429,6 @@ Cone::onSurface(const Geometry::Vec3D& R) const
 }
 
 
-void
-Cone::writePOVRay(std::ostream&) const
-  /*!
-    Write out the cone class in a POV-Ray file format.
-    \param  :: Output Stream (required for multiple std::endl)
-  */
-{
-  ELog::EM<<"Cone::writePovRay: not implemented yet"<< ELog::endErr;
-}
-
   
 void
 Cone::write(std::ostream& OX) const
