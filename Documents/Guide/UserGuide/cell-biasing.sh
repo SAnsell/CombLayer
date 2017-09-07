bebc438c4925ff9efbafbc9a66abc16fa616939f
Konstantin Batkov <konstantin.batkov@esss.se>
2017-09-07 11:01:29 +0200

    WIP: cell-based biasing description

--- Documents/Guide/UserGuide/cell-biasing.sh
+++ Documents/Guide/UserGuide/cell-biasing.sh
@@ -2,8 +2,10 @@
 
 ./ess -r -photon 0.5 -C 1e-3 -sdefVoid -n 30000 -w \
     --weightEnergyType energy 0.1 0.95 1.0 0.85 10.0 0.5 100.0 0.4 5000.0 0.3 \
-    --weightPlane 'Vec3D(600,0,0)' 'Vec3D(0,1,0)' \
-    --weightPlane 'Vec3D(1600,0,0)' 'Vec3D(1,1,0)' \
+    --weightSource 'Vec3D(600,0,0)' \
+    --weightPlane  'Vec3D(600,0,0)' 'Vec3D(0,1,0)' \
+    --weightPlane  'Vec3D(1600,0,0)' 'Vec3D(1,1,0)' \
     --weightObject BBunkerWallMainWall1 TP1 1.0 0.15 1e-20 \
+    --weightObject LinacTSW2            SS0 0.0 1.0  0.9   1.0 2.0 \
     -T mesh free DOSE 'Vec3D(1465,-600,10)' 'Vec3D(3660,360,20)' 30 60 50 \
     --voidUnMask TA
