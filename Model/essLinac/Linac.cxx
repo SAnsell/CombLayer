1112fd3066d303c6c4a079d220875a32d791ab35
Konstantin Batkov <konstantin.batkov@esss.se>
2017-09-12 14:41:22 +0200

    TSW substracted only from the air cell (not from the linac walls anymore)

--- Model/essLinac/Linac.cxx
+++ Model/essLinac/Linac.cxx
@@ -351,8 +351,8 @@ Linac::buildTSW(Simulation& System) const
       std::shared_ptr<TSW>
         wall(new TSW(keyName,"TSW",i));
       OR.addObject(wall);
+      wall->addInsertCell(this->getCells("air"));
       wall->createAll(System,*this,12,13,14,15);
-      attachSystem::addToInsertControl(System,*this,*wall);
     }
   return;
 }
