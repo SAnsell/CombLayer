690c6d06bc5a685c3ed79f319e686e65aa7523c5
Konstantin Batkov <konstantin.batkov@esss.se>
2017-10-10 14:44:15 +0200

    cell biasing made for ODIN

--- Documents/Guide/UserGuide/cell-biasing.sh
+++ Documents/Guide/UserGuide/cell-biasing.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-./ess -r -defaultConfig Single NMX -angle free 60 \
+./ess -r -defaultConfig Single ODIN -angle objAxis odinAxis 0 \
     -w \
-    --weightSource 'Vec3D(200,0,0)' \
-    --weightPlane  'Vec3D(200,0,0)'  'Vec3D(1,0,0)' \
+    --weightSource 'Vec3D(200.0,4.0,13.7)' \
     --weightPlane  'Vec3D(2800,0,0)' 'Vec3D(1,0,0)' \
     --weightEnergyType energy 0.1 0.95 1.0 0.85 10.0 0.5 100.0 0.4 5000.0 0.3 \
-    --weightObject G1BLineTop1          SS0 0.0 1.0 0.9 1.0 2.0 \
-    --weightObject ABunkerWallMainWall0 TP1 0.0 1.0 0.9 1.0 2.0 \
-    --weightObject ABunkerWallMainWall1 TP1 0.0 1.0 0.9 1.0 2.0 \
+    --weightObject G2BLineTop20         SS0 0.0 1.0 0.9 1.0 2.0 \
+    --weightObject CBunkerWallMainWall1 TP0 0.0 1.0 0.9 1.0 2.0 \
+    --weightObject CBunkerWallMainWall2 TP0 0.0 1.0 0.9 1.0 2.0 \
     TA
