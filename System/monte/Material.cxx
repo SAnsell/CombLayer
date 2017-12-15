383e0ab4aa1a1dda8c85d90a024bc8d6f58b57d7
Konstantin Batkov <konstantin.batkov@esss.se>
2017-12-15 15:41:06 +0100

    WIP: work on material card alignment

--- System/monte/Material.cxx
+++ System/monte/Material.cxx
@@ -871,19 +871,18 @@ Material::writeFLUKA(std::ostream& OX) const
   StrFunc::writeMCNPX(cx.str(),OX);
   cx.str("");
 
-  cx<<"MATERIAL 0.0 0.0 ";
-  cx<<atomDensity<<" "<<Mnum<<" 0.0 0.0 ";
-  cx<<"m"<<Mnum<<std::endl;
+  cx<<"MATERIAL  "<<std::setw(30)<<atomDensity<<std::setw(30)<<"m"+std::to_string(Mnum)<<std::endl;
   
   std::vector<Zaid>::const_iterator zc;
   std::vector<std::string>::const_iterator vc;
-  cx<<"COMPOUND ";
+  cx<<"COMPOUND  ";
   size_t i(0);
   for(const Zaid& ZItem: zaidVec)
     {
-    cx<<std::scientific<<ZItem.getDensity()<<" "<<ZItem.getZaidNum()<< " ";
+      cx<<std::setw(10)<<std::setprecision(3)<<ZItem.getDensity()<<
+	std::setw(10)<<"m"+std::to_string(ZItem.getZaidNum());
     i++;
-    if (!(i%3)) cx<<" m"<<Mnum<<std::endl<<"COMPOUND ";
+    if (!(i%3)) cx<<std::setw(10)<<" m"+std::to_string(Mnum)<<std::endl<<"COMPOUND  ";
     }
   cx<<std::endl;
 
