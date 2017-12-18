eb4c8d55b0f0f3c6b5c414bbe0bde9e748681608
Konstantin Batkov <konstantin.batkov@esss.se>
2017-12-18 17:54:28 +0100

    Tibor's copyright

--- Model/essBuild/F5Calc.cxx
+++ Model/essBuild/F5Calc.cxx
@@ -3,7 +3,7 @@
  
  * File:   essBuildInc/F5Calc.cxx
  *
- * Copyright (c) 2015 Konstantin Batkov
+ * Copyright (c) 2015-2017 Konstantin Batkov / Tibor Reiss
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
