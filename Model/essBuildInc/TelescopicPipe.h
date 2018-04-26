b36b0928eda1a00a378e8fcb6198d4c34aa45fb1
Konstantin Batkov <konstantin.batkov@esss.se>
2018-04-26 14:48:23 +0200

    TelescopicPipe copied from the master branch + clone() method added

--- Model/essBuildInc/TelescopicPipe.h
+++ Model/essBuildInc/TelescopicPipe.h
@@ -8,59 +8,54 @@ namespace essSystem
 
 /*!
   \class TelescopicPipe
-  \author A. Milocco
+  \author S. Ansell
   \version 1.0
   \date February 2013
-  \brief Telescopic pipe object
+  \brief Multi-component pipe
 */
+
 class TelescopicPipe : public attachSystem::ContainedGroup,
-    public attachSystem::FixedComp
+  public attachSystem::FixedOffset,
+  public attachSystem::FrontBackCut
 {
  private:
 
   const int ptIndex;              ///< Index of surface offset
   int cellIndex;                  ///< Cell index
-  int engActive; ///< True if engineering details should be built
-
-  double xStep;                   ///< X step
-  double yStep;                   ///< Y step
-  double zStep;                   ///< Z step
-  double xyAngle;                 ///< XY Angle
-  double zAngle;                  ///< Z Angle
 
   size_t nSec;                    ///< Number of tube sections
   std::vector<double> radius;     ///< Radius of inner void
-  std::vector<double> length;     ///< cummulative length
+  std::vector<double> length;     ///< cummolative length 
   std::vector<double> zCut;       ///< Z cut for section
-  std::vector<double> thick;      ///< Wall thickness
+  std::vector<double> thick;      ///< Wall thickness  
   std::vector<int> inMat;         ///< Inner material
   std::vector<int> wallMat;       ///< wall material
-  
+
   // Functions:
 
-  void populate(const FuncDataBase&);
   void createUnitVector(const attachSystem::FixedComp&,const long int);
 
   void createSurfaces();
-  void createObjects(Simulation&,const std::string&,const std::string&);
+  void createObjects(Simulation&);
   void createLinks();
+
+ protected:
+  void populate(const FuncDataBase&);
 
  public:
 
   TelescopicPipe(const std::string&);
   TelescopicPipe(const TelescopicPipe&);
   TelescopicPipe& operator=(const TelescopicPipe&);
-  virtual TelescopicPipe* clone() const;
+  virtual TelescopicPipe *clone() const;
   virtual ~TelescopicPipe();
-
-  void createAll(Simulation&,
-		 const attachSystem::FixedComp&,const long int,
-		 const attachSystem::FixedComp&,const long int,
-		 const attachSystem::FixedComp&);
-
+   
+  void createAll(Simulation&,const attachSystem::FixedComp&,
+		 const long int);
+ 
 };
 
 }
 
 #endif
-
+ 
