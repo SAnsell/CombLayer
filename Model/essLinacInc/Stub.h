38c55004b784ee065448b2a64a7cbaddbf2daf9d
Konstantin Batkov <konstantin.batkov@esss.se>
2018-05-21 15:53:33 +0200

    Stub class just built by cl-paster

---
+++ Model/essLinacInc/Stub.h
@@ -0,0 +1,81 @@
+/********************************************************************* 
+  CombLayer : MCNP(X) Input builder
+ 
+ * File:   essBuildInc/Stub.h
+ *
+ * Copyright (c) 2018 by Konstantin Batkov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *
+ ****************************************************************************/
+#ifndef essSystem_Stub_h
+#define essSystem_Stub_h
+
+class Simulation;
+
+namespace essSystem
+{
+
+/*!
+  \class Stub
+  \version 1.0
+  \author Konstantin Batkov
+  \date 21 May 2018
+  \brief Stub
+*/
+
+class Stub : public attachSystem::ContainedComp,
+  public attachSystem::FixedOffset
+{
+ private:
+
+  const int surfIndex;             ///< Index of surface offset
+  int cellIndex;                ///< Cell index
+
+  int engActive;                ///< Engineering active flag
+
+  double length;                ///< Total length including void
+  double width;                 ///< Width
+  double height;                ///< height
+
+  double wallThick;             ///< Thickness of wall
+
+  int mainMat;                   ///< main material
+  int wallMat;                   ///< wall material  
+  
+  void populate(const FuncDataBase&);
+  void createUnitVector(const attachSystem::FixedComp&,
+			const long int);
+  
+  void createSurfaces();
+  void createObjects(Simulation&);
+  void createLinks();
+  
+ public:
+
+  Stub(const std::string&);
+  Stub(const Stub&);
+  Stub& operator=(const Stub&);
+  virtual Stub* clone() const;
+  virtual ~Stub();
+  
+  void createAll(Simulation&,const attachSystem::FixedComp&,const long int);
+
+};
+
+}
+
+#endif
+ 
+
