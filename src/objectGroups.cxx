881ae1e2ebe5119ee766659f856e358db5d8f531
Stuart Ansell <xansell@gmail.com>
2018-09-25 16:00:18 +0200

    Removal of output

--- src/objectGroups.cxx
+++ src/objectGroups.cxx
@@ -353,7 +353,6 @@ objectGroups::renumberCell(const int oldCellN,
 {
   ELog::RegMethod RegA("objectGroups","renumberCell");
 
-  ELog::EM<<"od == "<<oldCellN<<" "<<newCellN<<ELog::endDiag;
   // first find if in active unit
   std::set<int>::iterator sc=activeCells.find(oldCellN);
   if (sc==activeCells.end())
