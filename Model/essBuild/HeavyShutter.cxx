9f5ec352dc04647cf7544a50a8334ab6e363fe21
amivaleo <jimmy.scionti@gmail.com>
2018-11-21 14:17:29 +0100

    fixing copyrights

--- Model/essBuild/HeavyShutter.cxx
+++ Model/essBuild/HeavyShutter.cxx
@@ -3,7 +3,7 @@
  
  * File:   essBuild/HeavyShutter.cxx
  *
- * Copyright (c) 2004-2018 by Stuart Ansell
+ * Copyright (c) 2017-2018 by Jimmy Scionti
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
