de80be4bd7630e7bbb7cace4c13b3725ebddfd4f
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2019-09-18 12:02:43 +0200

    WIP: Undulator length set to 300+4 cm

--- Model/MaxIV/softimax/softimaxVariables.cxx
+++ Model/MaxIV/softimax/softimaxVariables.cxx
@@ -102,7 +102,7 @@ undulatorVariables(FuncDataBase& Control,
   ELog::RegMethod RegA("softimaxVariables[F]","undulatorVariables");
   setVariable::PipeGenerator PipeGen;
 
-  const double L(210.0);
+  const double L(210.0+97);
   PipeGen.setMat("Aluminium");
   PipeGen.setNoWindow();   // no window
   PipeGen.setCF<setVariable::CF63>();
@@ -115,7 +115,7 @@ undulatorVariables(FuncDataBase& Control,
 
   // undulator I Vacuum
   Control.addVariable(undKey+"UndulatorVGap",1.1);  // mininum 11mm
-  Control.addVariable(undKey+"UndulatorLength",203.0); 
+  Control.addVariable(undKey+"UndulatorLength",203.0+97);  // 300 SA - check
   Control.addVariable(undKey+"UndulatorMagnetWidth",6.0);
   Control.addVariable(undKey+"UndulatorMagnetDepth",3.0);
   Control.addVariable(undKey+"UndulatorSupportWidth",12.0);
@@ -1072,7 +1072,7 @@ SOFTIMAXvariables(FuncDataBase& Control)
 
   // ystep / dipole pipe / exit pipe
   setVariable::R3FrontEndVariables
-    (Control,"SoftiMAXFrontBeam",310.0,724.0,40.0);
+    (Control,"SoftiMAXFrontBeam",310.0-97,724.0,40.0);
   softimaxVar::frontMaskVariables(Control,"SoftiMAXFrontBeam");
     
   softimaxVar::wallVariables(Control,"SoftiMAXWallLead");
