b423d9e3730d38a294fe591589528f497c028d84
stuans <xansell@gmail.com>
2019-11-14 10:11:25 +0100

    Fix of mirrorGen for formax

--- Model/MaxIV/formax/formaxVariables.cxx
+++ Model/MaxIV/formax/formaxVariables.cxx
@@ -320,7 +320,7 @@ mirrorBox(FuncDataBase& Control,const std::string& Name,
   MirrGen.setPrimaryAngle(0,vAngle,0);  
   // ystep : zstep : theta : phi : radius
   MirrGen.generateMirror(Control,Name+"MirrorFront"+Index,
-			 -centreDist/2.0,0.0,theta,phi,0.0);         // hits beam center
+			 0.0,-centreDist/2.0,0.0,theta,phi,0.0);         // hits beam center
   MirrGen.setPrimaryAngle(0,vAngle+180.0,0.0);
   // x/y/z/theta/phi/
   MirrGen.generateMirror(Control,Name+"MirrorBack"+Index,
