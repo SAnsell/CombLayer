b075d1548c14b1d9faa4222a2cc1b24dc31a86b1
Stuart Ansell <xansell@gmail.com>
2019-12-31 11:20:22 +0000

    Minr corrections to Danmax

--- Model/MaxIV/commonGenerator/CollGenerator.cxx
+++ Model/MaxIV/commonGenerator/CollGenerator.cxx
@@ -143,7 +143,7 @@ CollGenerator::setMinAngleSize(const double L,
    */
 {
   minLength=L;
-  minWidth= 2.0*Dist*tan(1e-6*angX/2.0);
+  minWidth = 2.0*Dist*tan(1e-6*angX/2.0);
   minHeight= 2.0*Dist*tan(1e-6*angZ/2.0);
   return;
 }
