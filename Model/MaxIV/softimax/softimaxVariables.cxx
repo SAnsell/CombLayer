4fbd76a4f99e15d21eee266d02c1e63d76c4b573
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2020-01-22 14:48:59 +0100

    ScreenA material set to Void

--- Model/MaxIV/softimax/softimaxVariables.cxx
+++ Model/MaxIV/softimax/softimaxVariables.cxx
@@ -955,7 +955,8 @@ shieldVariables(FuncDataBase& Control,
 
   ShieldGen.setPlate(75.0,75.0,7.0);
   ShieldGen.setWall(0.1,0.0);
-  ShieldGen.setMaterial("Stainless304","Stainless304","Void");
+  //  ShieldGen.setMaterial("Stainless304","Stainless304","Void");
+  ShieldGen.setMaterial("Void","Void","Void");
 
   ShieldGen.generateShield(Control,shieldKey+"ScreenA",
   			   Geometry::Vec3D(10.0,32.4,0.0),0.0);
