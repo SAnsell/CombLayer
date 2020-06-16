f7d05252cb3646a2071ea55af0a0d7d761eacde6
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2020-06-16 17:33:23 +0200

    SQ commented out

--- System/support/polySupport.cxx
+++ System/support/polySupport.cxx
@@ -1,6 +1,6 @@
-/********************************************************************* 
+/*********************************************************************
   CombLayer : MCNP(X) Input builder
- 
+
  * File:   support/polySupport.cxx
  *
  * Copyright (c) 2004-2019 by Stuart Ansell
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  ****************************************************************************/
 #include <iostream>
@@ -31,8 +31,8 @@
 #include "Exception.h"
 #include "polySupport.h"
 
-/*! 
-  \file polySupport.cxx 
+/*!
+  \file polySupport.cxx
 */
 
 double
@@ -50,7 +50,7 @@ quad(const double aa,const double bb,const double cc,const double x)
   return aa*x*x+bb*x+cc;
 }
 
-size_t 
+size_t
 solveRealQuadratic(const double& a,const double& b,const double& c,
 		   std::pair<double,double>& OutAns)
 /*!
@@ -58,15 +58,15 @@ solveRealQuadratic(const double& a,const double& b,const double& c,
   \param a :: x^2 coeff
   \param b :: x coeff
   \param c :: const coeff
-  \param OutAns :: roots of the equation 
-  \return number of unique and real solutions 
+  \param OutAns :: roots of the equation
+  \return number of unique and real solutions
 */
 {
   double cf;
 
   OutAns.first=0.0;
   OutAns.second=0.0;
-  
+
   if (a==0.0)
     {
       if (b==0.0) return 0;
@@ -74,7 +74,7 @@ solveRealQuadratic(const double& a,const double& b,const double& c,
       OutAns.second= OutAns.first;
       return 1;
     }
-  
+
   cf=b*b-4*a*c;
   if (cf>=0)          /* Real Roots */
     {
@@ -84,25 +84,25 @@ solveRealQuadratic(const double& a,const double& b,const double& c,
       if (OutAns.first>OutAns.second)
 	std::swap(OutAns.first,OutAns.second);
       return (cf==0) ? 1 : 2;
-    } 
+    }
   return 0;
 }
 
-size_t 
+size_t
 solveRealQuadratic(const std::vector<double>& Coeff,
 		   std::pair<double,double>& OutAns)
 /*!
   Solves quadratic  \f[ Ax^2+Bx+C \f]. with only the real roots
   \param Coef :: iterator over all the coefficients in the order
   \f[ Ax^2+Bx+C \f].
-  \param OutAns :: roots of the equation 
-  \return number of unique and real solutions 
+  \param OutAns :: roots of the equation
+  \return number of unique and real solutions
 */
 {
   return solveRealQuadratic(Coeff[0],Coeff[1],Coeff[2],OutAns);
 }
 
-size_t 
+size_t
 solveQuadratic(const double& a,const double& b,const double& c,
 	       std::complex<double>& AnsA,std::complex<double>& AnsB)
 /*!
@@ -110,9 +110,9 @@ solveQuadratic(const double& a,const double& b,const double& c,
   \param a :: x^2 coeff
   \param b :: x coeff
   \param c :: const coeff
-  \param AnsA :: complex roots of the equation 
-  \param AnsB :: complex roots of the equation 
-  \return number of unique solutions 
+  \param AnsA :: complex roots of the equation
+  \param AnsB :: complex roots of the equation
+  \return number of unique solutions
 */
 {
   std::pair<std::complex<double>,std::complex<double> > OutAns;
@@ -122,37 +122,37 @@ solveQuadratic(const double& a,const double& b,const double& c,
   return res;
 }
 
-size_t 
+size_t
 solveQuadratic(const std::vector<double>& Coef,
 	       std::pair<std::complex<double>,
 			 std::complex<double> >& OutAns)
 /*!
-  Solves Complex Quadratic 
+  Solves Complex Quadratic
   \param Coef :: iterator over all the coefients in the order
   \f[ Ax^2+Bx+C \f].
-  \param OutAns :: complex roots of the equation 
-  \return number of unique solutions 
+  \param OutAns :: complex roots of the equation
+  \return number of unique solutions
 */
 {
   return solveQuadratic(Coef[0],Coef[1],Coef[2],OutAns);
-			
+
 }
 
-size_t 
+size_t
 solveQuadratic(const double* D,
 	       std::pair<std::complex<double>,
 			 std::complex<double> >& OutAns)
 /*!
   Solves Complex Quadratic   \f[ Ax^2+Bx+C \f].
   \param D :: Parameter array (a,b,c)
-  \param OutAns :: complex roots of the equation 
-  \return number of unique solutions 
+  \param OutAns :: complex roots of the equation
+  \return number of unique solutions
 */
 {
   return solveQuadratic(D[0],D[1],D[2],OutAns);
 }
 
-size_t 
+size_t
 solveQuadratic(const double& a,const double& b,const double& c,
 	       std::pair<std::complex<double>,
 	                 std::complex<double> >& OutAns)
@@ -161,8 +161,8 @@ solveQuadratic(const double& a,const double& b,const double& c,
   \param a :: x^2 coeff
   \param b :: x coeff
   \param c :: const coeff
-  \param OutAns :: complex roots of the equation 
-  \return number of unique solutions 
+  \param OutAns :: complex roots of the equation
+  \return number of unique solutions
 */
 {
   double cf;
@@ -170,7 +170,7 @@ solveQuadratic(const double& a,const double& b,const double& c,
   if (a==0.0)
     {
       if (b==0.0)
-	{ 
+	{
 	  OutAns.first=std::complex<double>(0.0,0.0);
 	  OutAns.second=std::complex<double>(0.0,0.0);
 	  return 0;
@@ -189,7 +189,7 @@ solveQuadratic(const double& a,const double& b,const double& c,
       OutAns.first=std::complex<double>(q/a,0.0);
       OutAns.second=std::complex<double>(c/q,0.0);
       return (cf==0) ? 1 : 2;
-    } 
+    }
 
   const std::complex<double> CQ(-0.5*b,
 			  ((b>=0) ? -0.5*sqrt(-cf) :
@@ -208,10 +208,10 @@ solveCubic(const std::vector<double>& DVec,
   Solves Cubic equation
   Coef of eqn: \f[ Ax^3+Bx^2+Cx+D \f].
   \param DVec :: Parameters x^3 to const
-  \param AnsA :: complex roots of the equation 
-  \param AnsB :: complex roots of the equation 
-  \param AnsC :: complex roots of the equation 
-  \return number of unique solutions 
+  \param AnsA :: complex roots of the equation
+  \param AnsB :: complex roots of the equation
+  \param AnsC :: complex roots of the equation
+  \return number of unique solutions
 */
 {
   return solveCubic(DVec[0],DVec[1],DVec[2],DVec[3],AnsA,AnsB,AnsC);
@@ -229,10 +229,10 @@ solveCubic(const double* D,
   Iiterator over all the coefficients in the order
   \f[ Ax^3+Bx^2+Cx+D \f].
   \param D :: Parameters x^3 to const
-  \param AnsA :: complex roots of the equation 
-  \param AnsB :: complex roots of the equation 
-  \param AnsC :: complex roots of the equation 
-  \return number of unique solutions 
+  \param AnsA :: complex roots of the equation
+  \param AnsB :: complex roots of the equation
+  \param AnsC :: complex roots of the equation
+  \return number of unique solutions
 */
 {
   return solveCubic(D[0],D[1],D[2],D[3],AnsA,AnsB,AnsC);
@@ -243,7 +243,7 @@ solveCubic(const double A,const double B,
 	   const double C,const double D,
 	   std::complex<double>& AnsA,
 	   std::complex<double>& AnsB,
-	   std::complex<double>& AnsC) 
+	   std::complex<double>& AnsC)
   /*!
     Solves Cubic equation of type
     \f$ ax^3+bx^2+cx+d=0 \f$
@@ -251,13 +251,13 @@ solveCubic(const double A,const double B,
     \param B :: x^2 value
     \param C :: x value
     \param D :: const value
-    \param AnsA :: complex roots of the equation 
-    \param AnsB :: complex roots of the equation 
-    \param AnsC :: complex roots of the equation 
-    \return number of unique solutions 
+    \param AnsA :: complex roots of the equation
+    \param AnsB :: complex roots of the equation
+    \param AnsC :: complex roots of the equation
+    \return number of unique solutions
   */
 {
-  double q,r;        // solution parameters 
+  double q,r;        // solution parameters
   double s,t,termR,termI,discrim;
   double q3,r13;
 
@@ -267,18 +267,18 @@ solveCubic(const double A,const double B,
   const double b = B/A;
   const double c = C/A;
   const double d = D/A;
-  std::pair<std::complex<double>,std::complex<double> > SQ;
-  
+  //  std::pair<std::complex<double>,std::complex<double> > SQ;
+
   q = (3.0*c - (b*b))/9.0;                   // -q
-  r = -27.0*d + b*(9.0*c - 2.0*b*b);       // -r 
+  r = -27.0*d + b*(9.0*c - 2.0*b*b);       // -r
   r /= 54.0;
- 
-  discrim = q*q*q + r*r;           // r^2-qq^3 
-  // The first root is always real. 
+
+  discrim = q*q*q + r*r;           // r^2-qq^3
+  // The first root is always real.
   termR = (b/3.0);
 
-  if (discrim > 1e-13)  // one root real, two are complex 
-    { 
+  if (discrim > 1e-13)  // one root real, two are complex
+    {
       s = r + sqrt(discrim);
       s = ((s < 0) ? -pow(-s, (1.0/3.0)) : pow(s, (1.0/3.0)));
       t = r - sqrt(discrim);
@@ -293,9 +293,9 @@ solveCubic(const double A,const double B,
       return 3;
     }
 
-  // The remaining options are all real 
+  // The remaining options are all real
 
-  if (discrim< -1e-13) // All roots real and different 
+  if (discrim< -1e-13) // All roots real and different
     {
       q = -q;
       q3 = q*q*q;
@@ -304,11 +304,11 @@ solveCubic(const double A,const double B,
       AnsA=std::complex<double>(-termR + r13*cos(q3/3.0),0.0);
       AnsB=std::complex<double>(-termR + r13*cos((q3 + 2.0*M_PI)/3.0),0.0);
       AnsC=std::complex<double>(-termR + r13*cos((q3 - 2.0*M_PI)/3.0),0.0);
-	    
+
       return 3;
     }
 
-  // Only option left is that all roots are real and unequal 
+  // Only option left is that all roots are real and unequal
   // (to get here, q*q*q=r*r)
 
   r13 = ((r < 0) ? -pow(-r,(1.0/3.0)) : pow(r,(1.0/3.0)));
