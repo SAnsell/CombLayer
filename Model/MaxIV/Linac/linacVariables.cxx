204c3ffe3911bfc33055076e1879101dbd00c734
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2021-09-06 08:38:28 +0200

    SPFMazeLayerThick set to 5 cm

--- Model/MaxIV/Linac/linacVariables.cxx
+++ Model/MaxIV/Linac/linacVariables.cxx
@@ -3355,7 +3355,7 @@ wallVariables(FuncDataBase& Control,
   Control.addVariable(wallKey+"SPFMazeWidthSPF",160.0); // K_20-1_08G6b3
   Control.addVariable(wallKey+"SPFMazeLength",360.0); // derived from K_20-1_08G6b3: 690.0-330.0
 
-  Control.addVariable(wallKey+"SPFMazeLayerThick",2.0);
+  Control.addVariable(wallKey+"SPFMazeLayerThick",5.0);
   Control.addVariable(wallKey+"SPFMazeLayerMat","B4C");
   Control.addVariable(wallKey+"SPFMazeLayerHeight",256.0); // HQ, 2021-06-21
   Control.addVariable(wallKey+"SPFMazeLayerPipesWidth",150.0); // guess based on email HQ, 2021-06-21
