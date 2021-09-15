b0cde42f544d55ef321972f20ba459ddf76bd804
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2021-09-15 21:59:47 +0200

    TDC29BeamStop[AB] variables as of TBeamStop #18

--- Model/MaxIV/Linac/magnetVar.cxx
+++ Model/MaxIV/Linac/magnetVar.cxx
@@ -346,36 +346,11 @@ Segment29Magnet(FuncDataBase& Control,
   YagScreenGen.generateScreen(Control,lKey+"YagScreenB",0);   // closed
   Control.addVariable(lKey+"YagScreenBYAngle",-90.0);
 
-  BSGen.setFullLen(125.0);
+  BSGen.setFullLen(115.0);
   BSGen.generateBStop(Control,lKey+"BeamStopA",3.0);
-  BSGen.setFullLen(122.0);
   BSGen.generateBStop(Control,lKey+"BeamStopB",3.0);
   Control.addVariable(lKey+"BeamStopAYAngle",-90.0);
   Control.addVariable(lKey+"BeamStopBYAngle",-90.0);
-  
-  Control.addVariable(lKey+"BeamWingAYStep",100.0);
-  Control.addVariable(lKey+"BeamWingAXStep",25.0);
-  Control.addVariable(lKey+"BeamWingAPreZAngle",70.0);
-  Control.addVariable(lKey+"BeamWingAWidth",120.0);
-  Control.addVariable(lKey+"BeamWingADepth",10.0);
-  Control.addVariable(lKey+"BeamWingAHeight",200.0);
-  Control.addVariable(lKey+"BeamWingAMainMat","Poly");
-  
-  Control.addVariable(lKey+"BeamWingBYStep",100.0);
-  Control.addVariable(lKey+"BeamWingBXStep",-25.0);
-  Control.addVariable(lKey+"BeamWingBPreZAngle",-70.0);
-  Control.addVariable(lKey+"BeamWingBWidth",120.0);
-  Control.addVariable(lKey+"BeamWingBDepth",10.0);
-  Control.addVariable(lKey+"BeamWingBHeight",200.0);
-  Control.addVariable(lKey+"BeamWingBMainMat","Poly");
-
-    
-  BXGen.generateBox(Control,lKey+"BeamBoxA",3.0);
-  Control.addVariable(lKey+"BeamBoxAPreYAngle",-90.0);
-
-  BXGen.generateBox(Control,lKey+"BeamBoxB",3.0);
-  Control.addVariable(lKey+"BeamBoxBPreYAngle",-90.0);
-  Control.addVariable(lKey+"BeamBoxBZStep",-100.0);
 
   // end void
   Control.addVariable(lKey+"EndVoidLength",3.0);
