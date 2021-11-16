f43b3bbfd2440e34fc1864dd84c9c5041e2623e8
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2021-11-16 17:06:58 +0100

    variables from config. 34 used

--- Model/MaxIV/Linac/magnetVar.cxx
+++ Model/MaxIV/Linac/magnetVar.cxx
@@ -348,10 +348,14 @@ Segment29Magnet(FuncDataBase& Control,
   TDCBDGen.generate(Control,lKey+"BeamStopA");
   Control.addVariable(lKey+"BeamStopAYAngle",-90.0);
   Control.addVariable(lKey+"BeamStopAYStep",4.0);
+  Control.addVariable(lKey+"BeamStopABackSkinThick",0.0);
+  Control.addVariable(lKey+"BeamStopAPreCoreLength",47.0);
 
   TDCBDGen.generate(Control,lKey+"BeamStopB");
   Control.addVariable(lKey+"BeamStopBYAngle",-90.0);
   Control.addVariable(lKey+"BeamStopBYStep",4.0);
+  Control.addVariable(lKey+"BeamStopBBackSkinThick",0.0);
+  Control.addVariable(lKey+"BeamStopBPreCoreLength",47.0);
 
   // end void
   Control.addVariable(lKey+"EndVoidLength",3.0);
