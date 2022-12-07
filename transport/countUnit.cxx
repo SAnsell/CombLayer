6d8699c080b2d8473350917a4c9aef8b144cf059
Stuart Ansell <xansell@gmail.com>
2022-12-07 21:12:15 +0100

    Update for transport [start]

---
+++ transport/countUnit.cxx
@@ -0,0 +1,197 @@
+/********************************************************************* 
+  CombLayer : MCNP(X) Input builder
+ 
+ * File:   transport/countUnit.cxx
+ *
+ * Copyright (c) 2004-2022 by Stuart Ansell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *
+ ****************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+#include "Tensor.h"
+
+template<typename T>
+countUnit::Tensor() :
+  nx(0),ny(0),nz(0),U(0)
+  /*!
+    Default constructor
+  */
+{}
+
+countUnit::countUnit(const size_t NI,const size_T NE) :
+  nIndex(NI),nEBin(NE)
+{
+  if (nIndex*nEBin<1)
+    {
+      if (!nIndex)
+	throw ColErr::EmptyValue("countUnit::nIndex");
+      else
+	throw ColErr::EmptyValue("countUnit::nEBin");
+    }
+
+  setMem();
+  zeroMem();
+}
+
+countUnit::countUnit(const countUnit& A) :
+  nIndex(A.nIndex),nEBin(A.nIndex)
+  /*!
+    Copy Constructor
+    \param A :: Tensor to copy
+  */
+{
+  setMem();
+  copyMem(A);
+}
+
+countUnit::countUnit(const countUnit& A) :
+  nIndex(A.nIndex),nEBin(A.nIndex)
+  /*!
+    Copy Constructor
+    \param A :: Tensor to copy
+  */
+{
+  setMem();
+  copyMem(A);
+}
+
+countUnit::countUnit(const countUnit&& A) :
+  nIndex(A.nIndex),nEBin(A.nIndex)
+  /*!
+    Copy Constructor
+    \param A :: Tensor to copy
+  */
+{
+  A.data=nullptr;
+  A.cnt=nullptr;
+}
+
+countUnit&
+countUnit::operator=(const countUnit& A) 
+  /*!
+    Assignment operator
+    \param A :: data to copy
+    \return *this
+  */
+{
+  if (this!=&A)
+    {
+      if (nIndex!=A.nIndex || nEBin!=A.nEBin)
+	{
+	  if (nIndex!=A.nIndex)
+	    throw ColErr::IndexError<size_t>
+	      (nIndex,A.nIndex,"countUnit::nIndex");
+	  else
+	    throw ColErr::IndexError<size_t>
+	      (nEBin,A.nEBin,"countUnit::nEBin");
+	}
+      copyMem(A);
+    }
+  return *this;
+}
+
+countUnit&
+countUnit::operator=(const countUnit&& A) 
+  /*!
+    Move Assignment operator
+    \param A :: data to copy
+    \return *this
+  */
+{
+  if (this!=&A)
+    {
+      if (nIndex!=A.nIndex || nEBin!=A.nEBin)
+	{
+	  if (nIndex!=A.nIndex)
+	    throw ColErr::IndexError<size_t>
+	      (nIndex,A.nIndex,"countUnit::nIndex");
+	  else
+	    throw ColErr::IndexError<size_t>
+	      (nEBin,A.nEBin,"countUnit::nEBin");
+	}
+      data=std::move(A.data);
+      cnt=std::move(A.cnt);
+      A.data=nullptr;
+      A.cnt=nullptr;
+    }
+  return *this;
+}
+
+
+countUnit::~countUnit() 
+  /*!
+    Destructor 
+  */
+{
+  if (data)   // MUST CHECK: std::move will set nullptr
+    {
+      delete [] data[0];
+      delete data;
+      delete cnt;
+    }
+}
+
+void
+countUnit::copyMem(const countUnit& A)
+  /*!
+    simple function to copy blocks of memory
+    Will be sped up after debug
+  */
+{
+  for(size_t i=0;i<nIndex;i++)
+    for(size_t j=0;i<nEBin;i++)
+      data[i][j]=A.data[i][j];
+  for(size_t i=0;i<nIndex;i++)
+    cnt[i]=A.cnt[i];
+  return;
+}
+
+void
+countUnit::zeroMem()
+  /*!
+    Zero all the data
+    (to be sped up after debug)
+   */
+{
+  for(size_t i=0;i<nIndex;i++)
+    for(size_t j=0;j<nEBin;j++)
+      data[i][j]=0.0;
+  for(size_t i=0;i<nIndex;i++)
+    cnt[i]=0.0;
+  return;
+}
+
+void
+countUnit::setMem()
+  /*! 
+    Sets the memory held in data
+   */
+{
+  double* tmpX=new double[nIndex*nEBin];
+  data=new double*[nIndex];
+  for (size_t i=0;i<nIndex;i++)
+    data[i]=tmpX + (i*nEBin);
+  cnt=new double[nEBin];
+  return;
+}
+
+
+
---
+++ transportInc/ChannelDet.h
@@ -0,0 +1,78 @@
+/********************************************************************* 
+  CombLayer : MCNP(X) Input builder
+ 
+ * File:   transportInc/ChannelDet.h
+ *
+ * Copyright (c) 2004-2022 by Stuart Ansell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *
+ ****************************************************************************/
+#ifndef Transport_ChannelDet_h
+#define Transport_ChannelDet_h
+
+namespace MonteCarlo
+{
+  class particle;
+}
+
+namespace Transport
+{
+
+/*!  
+  \class ChannelDet
+  \brief Defines a detector channel for data
+  \version 1.0
+  \author S. Ansell
+  \date December 2022
+
+  This is constructed to ahve a fixed set of names
+  and a fixed set of energy bins
+*/
+
+class ChannelDet
+{
+ protected:
+
+  /// main unit name
+  const std::map<std::string,long int> unit;
+  const std::map<std::string,long int> ;
+  
+  const size_t index;         ///< Index number
+  size_t nps;                 ///< Number of points
+
+ public:
+  
+  explicit ChannelDet(const size_t);
+  ChannelDet(const ChannelDet&);
+  ChannelDet& operator=(const ChannelDet&);
+  virtual ChannelDet* clone() const =0;
+  virtual ~ChannelDet();
+  
+  virtual double project(const MonteCarlo::particle&,
+		       MonteCarlo::particle&) const =0;
+  virtual void addEvent(const MonteCarlo::particle&) =0;
+
+  virtual void clear() =0;
+  virtual void normalize(const size_t) {}
+  virtual void writeHeader(std::ostream&) const {}
+  virtual void write(std::ostream&) const =0;
+};
+
+std::ostream&
+operator<<(std::ostream&,const ChannelDet&);
+
+}  // NAMESPACE Transport
+
+#endif
