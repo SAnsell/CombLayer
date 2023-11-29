3596a4478a7a4cb2bda8759817cedfab65c088c7
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2023-11-29 16:18:43 +0100

    GTF POV-Ray scripts added

---
+++ povray/gtf.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [ITEM]"
+    echo "       ITEM is the value of ITEM in povray/tdc.pov"
+    echo "       Example: povray/gtf.sh Gate"
+    exit 1
+fi
+
+test -e maxiv || make -j$(nproc) maxiv
+
+ITEM=${BASH_ARGV} # last argument is the view in povray/tdc.pov
+
+params=" +A +W800 +H600 "
+void=""
+
+echo $ITEM
+echo ${segments}
+echo $void
+
+./maxiv -defaultConfig Single GunTestFacility -povray $void a \
+    && povray ${params} povray/gtf.pov <<< \"$ITEM\" && exit 0
