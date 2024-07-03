c12721037620ece1a481063db6df9bc24f901702
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2024-07-03 14:07:40 +0200

    all.sh checks surface estimators

--- all.sh
+++ all.sh
@@ -5,25 +5,40 @@ exec >/dev/null
 nValid=1000
 opts="--validAll --validCheck $nValid"
 # parallel options
-popts="-u --bar --halt now,fail=1"
+parallel="parallel -u --bar --halt now,fail=1"
 
-parallel $popts ::: \
+# FLUKA estimators
+# the uq is a perl replacement string to unquote the argument
+$parallel ./maxiv -fluka infn -defaultConfig Linac '{=1 uq(); =}' ::: \
+ " -T floor resnuc InjectionHall:Floor Concrete AA" \
+ " -T floor resnuc InjectionHall:Floor AA" \
+ " -T spect surface electron InjectionHall back AA" || exit
+
+# MCNP tallies
+$parallel ./maxiv -defaultConfig Linac '{=1 uq(); =}' ::: \
+	 " -T spect surface electron object     InjectionHall back AA" \
+	 " -T spect surface electron surfMap    InjectionHall InnerBack  1 AA" \
+	 " -T spect surface electron surfMap    InjectionHall InnerBack -1 AA" \
+	 " -T spect surface electron viewObject InjectionHall front BackWallFront \#BackWallBack '#SPFMazeIn' AA"  || exit
+
+# GEOMETRY
+$parallel ::: \
 	 "./maxiv --noLengthCheck --defaultConfig Linac All $opts AA" \
 	 "./ess --bunkerPillars ABunker $opts AA" \
 	 "./ess --topModType Butterfly $opts AA" \
 	 "./ess --topModType Pancake   $opts AA" || exit
 
-parallel $popts "./maxiv --defaultConfig Single {} $opts AA " ::: \
+$parallel "./maxiv --defaultConfig Single {} $opts AA " ::: \
    SOFTIMAX BALDER COSAXS DANMAX FORMAX MICROMAX SPECIES MAXPEEM || exit
 
-parallel $popts "./{} $opts AA" ::: t1Real reactor saxsSim || exit
+$parallel "./{} $opts AA" ::: t1Real reactor saxsSim || exit
 
 parallel --bar $popts "./ess --defaultConfig Single {} $opts AA" ::: \
  VESPA ESTIA CSPEC  ODIN MAGIC BIFROST LOKI NMX  NNBAR  DREAM  BEER   \
  FREIA SKADI MIRACLES TESTBEAM TREX VOR    || exit
 # HEIMDAL :: Not currently correct -- update underway
 
-parallel $popts "./singleItem --singleItem {} $opts AA" ::: \
+$parallel "./singleItem --singleItem {} $opts AA" ::: \
  BeamDivider BeamScrapper Bellow BlankTube BoxJaws         \
  BremBlock BremTube  ButtonBPM CRLTube  CeramicGap CleaningMagnet  \
  CollTube ConnectorTube CooledScreen CooledUnit CornerPipe \
