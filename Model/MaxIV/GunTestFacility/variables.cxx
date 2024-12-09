b97c33163885511cec9e3155939ca4bcf3157703
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2024-12-09 14:04:02 +0100

    change DuctWater1ShieldWidthRight to 30 cm (fixed with AR 5 dec)

--- Model/MaxIV/GunTestFacility/variables.cxx
+++ Model/MaxIV/GunTestFacility/variables.cxx
@@ -241,7 +241,7 @@ namespace setVariable
     Control.addVariable(name+"DuctWater1ShieldDepth",9.5); // -0.5 -> 10 cm from the floor; 9.5 -> until the floor
     Control.addVariable(name+"DuctWater1ShieldHeight",40);
     Control.addVariable(name+"DuctWater1ShieldWidthLeft",30);
-    Control.addVariable(name+"DuctWater1ShieldWidthRight",40.0);
+    Control.addVariable(name+"DuctWater1ShieldWidthRight",30.0);
     Control.addVariable(name+"DuctWater1ShieldWallOffset",15.0);
     Control.addVariable(name+"DuctWater1ShieldPenetrationWidth",200);
     Control.addVariable(name+"DuctWater1ShieldPenetrationHeight",8);
