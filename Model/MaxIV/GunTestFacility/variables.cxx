a3ecaaf374a608d449ff33a0ab087ed0e45e8f66
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2024-12-12 13:30:30 +0100

    adjust DuctSignal3FillDuct material/height

--- Model/MaxIV/GunTestFacility/variables.cxx
+++ Model/MaxIV/GunTestFacility/variables.cxx
@@ -235,9 +235,9 @@ namespace setVariable
 
     DuctGen.setSize(8.0); // [4]
     DuctGen.generate(Control,name+"DuctSignal3","Cylinder","None","None",90.0, 0.0, length2-189.0, 11.5-depth); // [0], pages 2, 4
-    Control.addVariable(name+"DuctSignal3DuctMat","StbTCABL");
+    Control.addVariable(name+"DuctSignal3DuctMat","StbTCABL%Void%36");
     Control.addVariable(name+"DuctSignal3FillMat","Sand%Void%75");
-    Control.addVariable(name+"DuctSignal3FillDuctHeightRatio",1.0-0.36);
+    Control.addVariable(name+"DuctSignal3FillDuctHeightRatio",1-0.1875);
     Control.addVariable(name+"DuctSignal3FillOffset",10.0);
     Control.addVariable(name+"DuctSignal3FillLength",50.0);
 
