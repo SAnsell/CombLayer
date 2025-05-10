2129bd71ba67c45e0a3a19627e5e84fc55aecf30
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2025-05-10 06:21:10 +0200

    PowerFilter generated with cl-paster

---
+++ Model/MaxIV/commonGeneratorInc/PowerFilterGenerator.h
@@ -0,0 +1,63 @@
+/*********************************************************************
+  CombLayer : MCNP(X) Input builder
+
+ * File:   Model/MaxIV/commonBeamInc/PowerFilterGenerator.h
+ *
+ * Copyright (c) 2004-2025 by Konstantin Batkov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************/
+#ifndef setVariable_PowerFilterGenerator_h
+#define setVariable_PowerFilterGenerator_h
+
+class FuncDataBase;
+
+namespace setVariable
+{
+
+/*!
+  \class PowerFilterGenerator
+  \version 1.0
+  \author Konstantin Batkov
+  \date May 2025
+  \brief PowerFilterGenerator for variables
+*/
+
+class PowerFilterGenerator
+{
+ private:
+
+  double length;                ///< Total length including void
+  double width;                 ///< Width
+  double height;                ///< Height
+  double wallThick;             ///< Wall thickness
+
+  std::string mainMat;          ///< Main material
+  std::string wallMat;          ///< Wall material
+
+ public:
+
+  PowerFilterGenerator();
+  PowerFilterGenerator(const PowerFilterGenerator&);
+  PowerFilterGenerator& operator=(const PowerFilterGenerator&);
+  virtual ~PowerFilterGenerator();
+
+  virtual void generate(FuncDataBase&,const std::string&) const;
+
+};
+
+}
+
+#endif
