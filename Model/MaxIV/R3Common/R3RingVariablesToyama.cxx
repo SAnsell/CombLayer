0f0cea6fa902523f1b7451364d88ccfd38965b36
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2025-11-13 16:58:14 +0100

    Remove BellowDA
    It does not exist neither in DanMAX (S0-2-0AB01088_DanMAX.pdf) nor in TomoWISE (Toyama_Formax_FE_Mechanical_drawings.pdf)

--- Model/MaxIV/R3Common/R3RingVariablesToyama.cxx
+++ Model/MaxIV/R3Common/R3RingVariablesToyama.cxx
@@ -110,8 +110,6 @@ moveApertureTableToyama(FuncDataBase& Control,
   PipeGen.generatePipe(Control,frontKey+"PipeB",15.0);
 
   BellowGen.setMat("SteelUnknownGrade", "SteelUnknownGrade");
-  BellowGen.setCF<setVariable::CF40>();
-  BellowGen.generateBellow(Control,frontKey+"BellowDA",10.0); // [2]
 
   BellowGen.setCF<setVariable::CF63>();
   BellowGen.generateBellow(Control,frontKey+"BellowE",14.0); // [2]
