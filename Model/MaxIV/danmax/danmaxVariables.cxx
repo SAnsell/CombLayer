614777dc2e102fffdd96a9cfbd0c015b1bbad36d
Udo Friman-Gayer <udo.friman-gayer@maxiv.lu.se>
2026-02-12 11:39:35 +0100

    Bellow A dimensions

--- Model/MaxIV/danmax/danmaxVariables.cxx
+++ Model/MaxIV/danmax/danmaxVariables.cxx
@@ -216,7 +216,8 @@ frontMaskVariables(FuncDataBase& Control,
   setVariable::BellowGenerator BellowGen;
   setVariable::FlangePlateGenerator FPGen;
 
-  Control.addVariable(preName+"BellowALength",10.0); // [4]
+  Control.addVariable(preName+"BellowALength",10.5); // [10]
+  Control.addVariable(preName+"BellowABellowStep",2.5); // [10]
 
   constexpr double FM1Length(40.0); // [4]
   constexpr double FM2Length(50.5); // [5]
