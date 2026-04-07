459dd52edef83acb2334825403126ad331381012
Udo Friman-Gayer <udo.friman-gayer@maxiv.lu.se>
2026-04-07 15:41:57 +0200

    Class docstring

--- Model/MaxIV/danmaxInc/CM2Crystal.h
+++ Model/MaxIV/danmaxInc/CM2Crystal.h
@@ -33,7 +33,40 @@ namespace xraySystem
   \date April 2026
   \brief Crystal in CM2 for catching and realigning the beam from CM1.
 
-  This class implements the crystal and its copper holder.
+  The purpose of the CM2 crystal is to redirect the Bragg-reflected beam from the CM1
+  splitter crystal to a direction that is parallel to the original optical axis and
+  constitutes the SINCRYS branch.
+  This class has been developed in close analogy to the CM1BeamSplitter class. The
+  reader is referred to that class for more details.
+
+  This class constructs the central piece inside the CM2 chamber: a copper holder on
+  which the crystal is mounted. Like the splitter crystal in CM1, the CM2 crystal has
+  a nontrivial 3D rotation to prevent specular reflection into the SINCRYS branch.
+  CM2 crystal provides a special mode for specular reflection along the beam path for
+  testing purposes.
+
+  As in CM1BeamSplitter, it is assumed that the beam travels in the positive Y
+  direction. In all modes, the element is built such that the Origin is at the center
+  of the crystal on its outer surface and that the crystal surface is (almost) the
+  X-Z plane.
+
+  This class uses the same convention for angles and coordinate axes as
+  CM1BeamSplitter, but note that the coordinate systems for CM1 and CM2 in Ref. [1]
+  are rotated against each other.
+
+  This class supports two positioning modes. For compatibility with CM1BeamSplitter,
+  these are called 2 and 3, because the SINCRYS branch is only active in these two 
+  modes. Note again that the origin is always at the same position. The two modes only
+  differ by a 3D rotation.
+
+  Mode 2: Bragg-reflection mode. The crystal is tilted to prevent specular reflection
+  into the SINCRYS branch.
+
+  Mode 3: Specular-reflection mode. The holder is oriented such that the crystal
+  surface is in the X-Z plane, i.e. the beam direction is perpendicular to the crystal
+  surface. This means that the pitch, roll, and yaw rotation of the crystal have been
+  compensated. By changing the yaw (ZAngle) of the holder, the specular-reflection
+  angle in the X-Y plane can be set in a straighforward way.
 
   References:
   [1] JJ X-RAY, SINCRYS beamline - MAXIV, Final Design Report v2, 23087, 2025-07-11
