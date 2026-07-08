aee528171681f1123363d8ebb954421cefb6f674
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2026-07-08 17:49:22 +0200

    all.sh generates individual decks

--- all.sh
+++ all.sh
@@ -2,49 +2,49 @@
 
 exec >/dev/null
 
-nValid=1000
+nValid=100
 opts="--validAll --validCheck $nValid"
 parallel="parallel -u --bar --halt now,fail=1"
 inp="/tmp/AA"
 
 # FLUKA estimators
 # the uq is a perl replacement string to unquote the argument
-$parallel ./maxiv -fluka infn -defaultConfig Linac -T '{=1 uq(); =}' $inp ::: \
- "myname resnuclei InjectionHall:Floor Concrete" \
- "myname resnuclei InjectionHall:Floor" \
- "myname surface electron InjectionHall back" \
- "myname surface 'e+&e-'  InjectionHall back 1e-11 3000 100 0 6.28318 3" \
- "myname surface electron InjectionHall back  -TMod energy myname 1e-11 3000 100" \
- "myname mesh dose-eq free 'Vec3D(-15.0,-400.0,-55.0)' 'Vec3D(15.0, 40.0,40.0)' 10 20 30 " \
- "myname mesh dose-eq free 'Vec3D(-15.0,-400.0,-55.0)' 'Vec3D(15.0, 40.0,40.0)' 10 20 30 -TMod doseType myname  all-part EWT74" \
- "help" "help resnuclei" "help surface"  || exit
+$parallel ./maxiv -fluka infn -defaultConfig Linac -T '{=1 uq(); =}' ::: \
+ "myname resnuclei InjectionHall:Floor Concrete ihf1" \
+ "myname resnuclei InjectionHall:Floor ihf2" \
+ "myname surface electron InjectionHall back ihf3" \
+ "myname surface 'e+&e-'  InjectionHall back 1e-11 3000 100 0 6.28318 3 ihf4" \
+ "myname surface electron InjectionHall back  -TMod energy myname 1e-11 3000 100 ihf5" \
+ "myname mesh dose-eq free 'Vec3D(-15.0,-400.0,-55.0)' 'Vec3D(15.0, 40.0,40.0)' 10 20 30 ihf6" \
+ "myname mesh dose-eq free 'Vec3D(-15.0,-400.0,-55.0)' 'Vec3D(15.0, 40.0,40.0)' 10 20 30 -TMod doseType myname  all-part EWT74 ihf7" \
+ "help ihf8" "help resnuclei ihf9" "help surface ihf10"  || exit
 
 # MCNP tallies
-$parallel ./maxiv -defaultConfig Linac -T myname '{=1 uq(); =}' $inp ::: \
-	 " surface e object     InjectionHall back" \
-	 " surface e surfMap    InjectionHall InnerBack  1" \
-	 " surface e surfMap    InjectionHall InnerBack -1" \
-	 " surface e viewObject InjectionHall front BackWallFront \#BackWallBack '#SPFMazeIn'" \
-	 " flux e InjectionHall:Floor Concrete" \
-	 " flux e InjectionHall:Floor All" || exit
+$parallel ./maxiv -defaultConfig Linac -T myname '{=1 uq(); =}' ::: \
+	 " surface e object     InjectionHall back ihm1" \
+	 " surface e surfMap    InjectionHall InnerBack  1 ihm2" \
+	 " surface e surfMap    InjectionHall InnerBack -1 ihm3" \
+	 " surface e viewObject InjectionHall front BackWallFront \#BackWallBack '#SPFMazeIn' ihm4" \
+	 " flux e InjectionHall:Floor Concrete ihm5" \
+	 " flux e InjectionHall:Floor All ihm6" || exit
 
 # GEOMETRY
-$parallel {} $inp ::: "./maxiv --noLengthCheck --defaultConfig Linac All $opts" \
-	  "./ess --bunkerPillars ABunker $opts" \
-	  "./ess --topModType Butterfly $opts" \
-	  "./ess --topModType Pancake   $opts" || exit
+$parallel {} ::: "./maxiv --noLengthCheck --defaultConfig Linac All $opts linac" \
+	  "./ess --bunkerPillars ABunker $opts ess" \
+	  "./ess --topModType Butterfly $opts butterfly" \
+	  "./ess --topModType Pancake   $opts pancake" || exit
 
-$parallel "./maxiv --defaultConfig Single {} $opts $inp " ::: \
+$parallel "./maxiv --defaultConfig Single {} $opts {} " ::: \
    SOFTIMAX BALDER COSAXS DANMAX FORMAX MICROMAX SPECIES MAXPEEM || exit
 
-$parallel "./{} $opts $inp" ::: t1Real reactor saxsSim || exit
+$parallel "./{} $opts {}" ::: t1Real reactor saxsSim || exit
 
-$parallel "./ess --defaultConfig Single {} $opts $inp" ::: \
+$parallel "./ess --defaultConfig Single {} $opts {}" ::: \
  VESPA ESTIA CSPEC  ODIN MAGIC BIFROST LOKI NMX  NNBAR  DREAM  BEER   \
  FREIA SKADI MIRACLES TESTBEAM TREX VOR    || exit
 # HEIMDAL :: Not currently correct -- update underway
 
-$parallel "./singleItem --singleItem {} $opts $inp" ::: \
+$parallel "./singleItem --singleItem {} $opts {}" ::: \
  BeamDivider BeamScrapper Bellow BlankTube BoxJaws         \
  BremBlock BremTube  ButtonBPM CRLTube  CeramicGap CleaningMagnet  \
  CollTube ConnectorTube CooledScreen CooledUnit CornerPipe \
